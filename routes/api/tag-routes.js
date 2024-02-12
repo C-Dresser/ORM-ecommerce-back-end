a7aa28749e10fd48037adcd4b1fc45437dfc84b6
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2024-02-12 16:09:19 -0500

    added tags routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,47 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+//-------------FIND ALL TAGS--------------//
 router.get('/', async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({include: [{model: Product,through: ProductTag}]}) 
+  .then(tags => {
+    res.status(200).json(tags) 
+  });
 });
 
+//-------------FIND TAG BY ID--------------//
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({ where: {id: req.params.id},include: [{model:Product, through: ProductTag}]}) // double check if this is right
+  .then(tags => {
+    res.status(200).json(tags)
+    res.json(tags) 
+  });
 });
 
+//-------------CREATE NEW TAG--------------//
 router.post('/', async (req, res) => {
-  // create a new tag
+  Tag.create(req.body)
+    .then((tag) => {
+      res.status(200).json(tag);
+    });
 });
 
+//-------------UPDATE TAG--------------//
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+    id: req.params.id
+   }
+ });
 });
 
+//-------------DELETE TAGS--------------//
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }  
+  });
 });
 
 module.exports = router;
