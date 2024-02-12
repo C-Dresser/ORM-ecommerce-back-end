db4080c36012d7de38729344ec8031790a092523
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2024-02-12 14:45:19 -0500

    added category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,48 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+//---------------FIND ALL CATEGORIES--------------//
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({ include: [Product] })
+    .then((categories) => {
+      res.json(categories);
+    });
 });
 
+//-------------FIND CATEGORY BY ID----------------//
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findByPk(req.params.id, { include: [Product] })
+    .then((category) => {
+      res.json(category);
+    });
 });
 
+//--------------CREATE NEW CATEGORY-------------//
 router.post('/', async (req, res) => {
-  // create a new category
+  Category.create(req.body)
+    .then((category) => {
+      res.status(200).json(category);
+    });
 });
 
+//------------UPDATE CATEGORY-------------//
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    req.body,
+    {
+      where: {
+        id: req.params.id,
+      },
+    });
 });
 
+//---------DELETE CATEGORY--------------//
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
 });
 
 module.exports = router;
